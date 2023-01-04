13e77c458b201c710355a3df1e17e5c3bd0788cf
Roger Esteve <rogeresteve30@gmail.com>
2023-01-04 04:47:18 +0100

    rating inside blocks

--- graph_generator.py
+++ graph_generator.py
@@ -1,4 +1,4 @@
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 import json
 import sys
 import math
@@ -22,10 +22,6 @@ block_w = 64
 def lerp(a, b, t):
     return a * (1 - t) + b * t
 
-def smooth_noise(a, b, t, remap):
-    tt = remap(t)
-    return lerp(a, b, tt)
-
 def load_series(series_id):
     f = open(series_id + ".json", "r")
     return json.loads(f.read())
@@ -35,7 +31,7 @@ def get_color(rating):
     c_a = COLORS[flr]
     c_b = COLORS[flr+1]
     t = rating-flr
-    
+
     r = int(lerp(c_a[0], c_b[0], t))
     g = int(lerp(c_a[1], c_b[1], t))
     return (r, g, 0)
@@ -54,13 +50,17 @@ if __name__ == '__main__':
     img_h = block_w*ep_height
 
     image = Image.new('RGB', (img_w, img_h))
+    
+    image_draw = ImageDraw.Draw(image)
+    font = ImageFont.truetype('arial', size=25)
 
     pixels = image.load()
     for xx in range(0, ep_width):
         for yy in range(0, len(data["data"][xx])):
-            color = get_color(data["data"][xx][yy])
+            rating = data["data"][xx][yy]
+            color = get_color(rating)
             for y in range(0, block_w):
                 for x in range(0, block_w):
-                    pixels[xx*block_w+x, yy*block_w+y] = color
-    
+                    pixels[xx*block_w+x, yy*block_w+y] = color    
+            image_draw.text((xx*block_w+15, yy*block_w+20), str(rating), font=font, fill =(0, 0, 0))
     image.show()
